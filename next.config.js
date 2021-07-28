e5dbdfafb5ec2f0ecc27d821e0316605a35c9f86
Andressa <andressa@MacBook-Pro-de-administrador.local>
2021-07-28 01:12:32 -0300

    feat: config testes

--- next.config.js
+++ next.config.js
@@ -1,3 +1,9 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 module.exports = {
   reactStrictMode: true,
 }
+
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
+module.exports = withBundleAnalyzer({})
